fae7b1e9ada027efc9b6b3c601192d1c461fcfd8
Igor Couto <igor.fcouto@gmail.com>
2018-08-07 03:20:13 -0300

    API now accept CORS

--- config/express.js
+++ config/express.js
@@ -8,6 +8,14 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join('./', 'views')));
 
+// Middleware to allow CORS
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+
 consign()
     .include('models')
     .then('routes')
